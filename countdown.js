3f36ce8058fb963f7adbeebf338a6e60427bcd75
Martijn Bleeker <martijn@keplar.nl>
2015-12-09 23:32:08 +0100

    WIP

--- countdown.js
+++ countdown.js
@@ -4,16 +4,32 @@
   var countDownDate, countTimeout;
   var countdownHolder, daysHolder, hoursHolder, minutesHolder, secondsHolder;
 
+  var minuteMilliseconds = 60000;
+  var hourMilliseconds = minuteMilliseconds * 60;
+  var dayMilliseconds = hourMilliseconds * 24;
+
   function pad2(number) {
     return (number < 10 ? "0" : "") + number;
   }
 
+  function dayRemainder(date) {
+    return date % dayMilliseconds;
+  }
+
+  function hourRemainder(date) {
+    return dayRemainder(date) % hourMilliseconds;
+  }
+
+  function minuteRemainder(date) {
+    return hourRemainder(date) % minuteMilliseconds;
+  }
+
   function countdown(futureDate) {
     var dd = futureDate - new Date();
-    var dday = Math.floor(dd / (60 * 60 * 1000 * 24));
-    var dhour = Math.floor((dd % (60 * 60 * 1000 * 24)) / (60 * 60 * 1000));
-    var dmin = Math.floor(((dd % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) / (60 * 1000));
-    var dsec = Math.floor((((dd % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) % (60 * 1000)) / 1000);
+    var dday = Math.floor(dd / dayMilliseconds);
+    var dhour = Math.floor(dayRemainder(dd) / hourMilliseconds);
+    var dmin = Math.floor(hourRemainder(dd) / minuteMilliseconds);
+    var dsec = Math.floor(minuteRemainder(dd) / 1000);
 
     daysHolder.text(pad2(Math.max(0, dday)));
     hoursHolder.text(pad2(Math.max(0, dhour)));
@@ -31,8 +47,8 @@
     }
   }
 
-  window.Countdown = function(time) {
-    countDownDate = time;
+  window.Countdown = function(date) {
+    countDownDate = date;
 
     if($(".js-countdown").length > 0) {
       countdownHolder = $(".js-countdown");
